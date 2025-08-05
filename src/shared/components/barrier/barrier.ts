e54704b04d89e02a0e90eecdf066f096870cf1a3
Bruno Robles <brunow.robles@hotmail.com>
2025-08-05 00:14:51 -0300

    feat: - added openBar method; - change some css styles;

--- src/shared/components/barrier/barrier.ts
+++ src/shared/components/barrier/barrier.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  inject,
+  Input,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-barrier',
   imports: [],
   templateUrl: './barrier.html',
-  styleUrl: './barrier.scss'
+  styleUrl: './barrier.scss',
 })
-export class Barrier {
+export class Barrier implements OnInit, AfterViewInit {
+  private el = inject(ElementRef);
+  private renderer = inject(Renderer2);
+  private canOpenBar: boolean = false;
 
+  /* @Input() set rotateBar(value: string) {
+    const target = this.el.nativeElement.querySelector('.bar');
+
+    if (target) this.renderer.setStyle(target, 'transform', `rotate(${value})`);
+  } */
+
+  ngOnInit() {
+    timer(5000).subscribe(() => this.openBar());
+  }
+
+  ngAfterViewInit() {}
+
+  openBar() {
+    // @TODO: Has to implement this validation;
+    //if (this.canOpenBar) {}
+
+    const target = this.el.nativeElement.querySelector('.bar');
+    if (target) this.renderer.setStyle(target, 'transform', `rotate(0deg)`);
+  }
 }
